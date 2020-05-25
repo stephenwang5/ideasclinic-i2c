59379b8667eecb73d191917542bdf4fbae4e4ff1
Stephen Wang <ste53539@gmail.com>
2020-05-24 21:59:49 -0400

    initial commit

---
+++ Core/Inc/i2c-communication.h
@@ -0,0 +1,12 @@
+
+#ifndef _I2C_COMMUNICATION_H_
+#define _I2C_COMMUNICATION_H_
+
+
+// Message definitions
+#define I2C_MESSAGE_CONFIRM 0x01
+#define I2C_MESSAGE_CONFIRM_SIZEOF 0x01
+
+
+
+#endif
---
+++ jetson/i2c.hpp
@@ -0,0 +1,3 @@
+#include "ros/ros.h"
+#include <fstream>
+#define I2C_DEVICE "/dev/i2c-0"
