81235b2dbb31321311ce71a2983ef3986564f0f1
Code-Titan <artemis.sotnikov12@gmail.com>
2020-05-26 21:50:55 -0400

    Basic float assmebly protocol
    Used unions to develop and test a function which assemblea float from 4 unisgned chars.

--- Core/Inc/i2c-communication.h
+++ Core/Inc/i2c-communication.h
@@ -7,6 +7,12 @@
 #define I2C_MESSAGE_CONFIRM 0x01
 #define I2C_MESSAGE_CONFIRM_SIZEOF 0x01
 
+// Union definition
+#include <stdio.h>
 
+union FloatConverter {
+	float float_result;
+	unsigned char bytes[4];
+};
 
 #endif
--- Core/Src/i2c-communication.c
+++ Core/Src/i2c-communication.c
@@ -1,1 +1,11 @@
+#include "i2c-communication.h"
 
+float fAssembleFloatFromBytes(unsigned char *byte_data) {
+	union FloatConverter converter;
+
+	for (int arr_idx = 0; arr_idx < 4; arr_idx++) {
+		converter.bytes[arr_idx] = byte_data[arr_idx];
+	}
+
+	return converter.float_result;
+};
